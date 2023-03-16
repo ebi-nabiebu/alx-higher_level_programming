76a910065f7032b30d365ff33e3d37770739198b
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2023-03-15 22:15:28 -0700

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,18 +4,19 @@ def roman_to_int(roman_string):
     """Function converts a Roman numeral to an integer."""
     if type(roman_string) is not str or roman_string is None:
         return 0
-    roman_dict = {
+    rom_dictionary = {
         'I': 1,
         'V': 5,
         'X': 10,
         'L': 50,
         'C': 100,
-        'D': 500,                                                        'M': 1000                                                      }
-        decs = [roman_dict[x] for x in roman_string]
-        output = 0
-        for i in range(len(decs)):
-            output += decs[i]
-                                                    if decs[i - 1] < decs[i] and i != 0:
-                                                                    output -= (decs[i - 1] + decs[i - 1])
-                                                                        return output
-
+        'D': 500,
+        'M': 1000
+    }
+    ebi_roms = [rom_dictionary[eb] for eb in roman_string]
+    result = 0
+    for e in range(len(ebi_roms)):
+        result += ebi_roms[e]
+        if ebi_roms[e - 1] < ebi_roms[e] and e != 0:
+            result -= (ebi_roms[e - 1] + ebi_roms[e - 1])
+    return result
