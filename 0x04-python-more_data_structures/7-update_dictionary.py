8e59ef4d0844ae1bc31f1f83217908ccb11f8396
Ebitari Nabiebu <ebi.nabiebu@gmail.com>
2023-03-15 21:26:21 -0700

    7.Update Dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(eb_dictionary, key, value):
+    """Function replaces or adds key/value in a dictionary."""
+    eb_dictionary[key] = value
+    return eb_dictionary
